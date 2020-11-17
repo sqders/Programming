380747f6f3205e87404523d629f45d956b25cb2c
sqders <sqders@mail.ru>
2020-11-17 20:08:56 +0300

    chabanov ne bei  glupogo ja zasnul i ne smog prosnutsa prostite

---
+++ practice/24/C++/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <json.hpp>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+	string id = "id",complited="true";
+
+	setlocale(LC_ALL, "Russian");
+	int mas[200000000];
+	string path_of_input = "vhod+vihod+header/in.json";
+
+	ifstream steam;
+	steam.open(path_of_input);
+	if (!steam.is_open())
+	{
+		cout << "error file not open"<<"\n";
+	}
+	else
+	{
+		cout << "file is open ,all is ok"<<"\n";
+		string str;
+		while (!steam.eof()) {
+			
+			getline(steam, str);
+			for (int i = 0; i < str.length(); i++)
+			{
+			
+			}
+		}
+	}
+
+
+}
